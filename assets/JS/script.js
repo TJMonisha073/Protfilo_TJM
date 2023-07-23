e5d18af3ef427c85e525c103d0cd67c30d26c146
TJMonisha073 <etanjimjahan@gmail.com>
2023-07-23 10:08:24 +0600

    first commit

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+// function contentvanis(){
+//    document.getElementsByClassName("navbar-nav").style.display='none';
+// }
+
+
+var typed = new Typed('#element', {
+    strings: ['Web And Interface Designer', 'Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    showCursor: false,
+});
